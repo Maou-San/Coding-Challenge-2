1d9c58f7d0292bad5477c88263c275a173c6792b
Dan-Andrei Gheorghe <Dan-Andrei.Gheorghe@softwire.com>
2014-07-02 17:53:25 +0100

    Modified Dictionary (still not in complete OO form) Began work on Challenge Solver

--- Coding Challenge 2/Program.cs
+++ Coding Challenge 2/Program.cs
@@ -1,6 +1,5 @@
 ﻿namespace Coding_Challenge_2
 {
-    using System;
     using System.Collections.Generic;
 
     internal class TlsDictionary
@@ -9,52 +8,53 @@
 
         public bool ContainsTls(char[] word)
         {
-            return (isTls(word) && dictionar.ContainsKey(word));
+            return (IsTls(word) && dictionar.ContainsKey(word));
         }
 
         public void AddTls(char[] word)
         {
-            if (isTls(word))
+            if (ContainsTls(word))
             {
-                if (ContainsTls(word))
-                {
-                    var count = 0;
-                    dictionar.TryGetValue(word, out count);
-                    count += 1;
-                    dictionar.Remove(word);
-                    dictionar.Add(word, count);
-                }
-                else
-                {
-                    dictionar.Add(word, 1);
-                }
+                var count = 0;
+                dictionar.TryGetValue(word, out count);
+                count += 1;
+                dictionar.Remove(word);
+                dictionar.Add(word, count);
             }
             else
             {
-                throw new Exception("Not a TLS");
+                dictionar.Add(word, 1);
             }
         }
 
         public int CountTls(char[] word)
         {
-            if (isTls(word))
+            var count = 0;
+            if (ContainsTls(word))
             {
-                var count = 0;
-                if (ContainsTls(word))
-                {
-                    dictionar.TryGetValue(word, out count);
-                    return count;
-                }
-                return 0;
+                dictionar.TryGetValue(word, out count);
+                return count;
             }
-            throw new Exception("Not a TLS");
+            return 0;
         }
 
-        private bool isTls(char[] word)
+        private bool IsTls(char[] word)
         {
-            return word.Length == 3;
+            return (word.Length == 3 && OnlyLowerLetters(word));
+        }
+
+        private bool OnlyLowerLetters(char[] word)
+        {
+            return (IsLowerLetter(word[0]) && IsLowerLetter(word[1]) && IsLowerLetter(word[2]));
+        }
+
+        private bool IsLowerLetter(char letter)
+        {
+            return ('a' <= letter && letter <= 'z');
         }
     }
+
+    internal class ChallengeSolver {}
 
     internal class Program
     {
