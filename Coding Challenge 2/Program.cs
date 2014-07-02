cfffd1edb08739f8aa4da978cbc3ece88cbce1f1
Dan-Andrei Gheorghe <Dan-Andrei.Gheorghe@softwire.com>
2014-07-02 18:41:35 +0100

    Modified TlsDictionary to have method that returns an array of all pairs contained in it.

--- Coding Challenge 2/Program.cs
+++ Coding Challenge 2/Program.cs
@@ -1,10 +1,32 @@
 ﻿namespace Coding_Challenge_2
 {
+    using System;
     using System.Collections.Generic;
+    using System.IO;
 
     internal class TlsDictionary
     {
-        private Dictionary<char[], int> dictionar;
+        private readonly Dictionary<char[], int> dictionar = new Dictionary<char[], int>();
+
+        public Tuple<char[], int>[] ArrayOfAllPairs
+        {
+            get
+            {
+                //To be modified s.t. the implementation is not visible --- make what follows next into a private method
+                Tuple<char[], int>[] arrayTuples;
+                var i = 0;
+                var count = 0;
+                var keyColl = dictionar.Keys;
+                arrayTuples = new Tuple<char[], int>[keyColl.Count];
+                foreach (var s in keyColl)
+                {
+                    dictionar.TryGetValue(s, out count);
+                    arrayTuples[i] = Tuple.Create(s, count);
+                    i += 1;
+                }
+                return arrayTuples;
+            }
+        }
 
         public bool ContainsTls(char[] word)
         {
@@ -29,9 +51,9 @@
 
         public int CountTls(char[] word)
         {
-            var count = 0;
             if (ContainsTls(word))
             {
+                var count = 0;
                 dictionar.TryGetValue(word, out count);
                 return count;
             }
@@ -54,10 +76,24 @@
         }
     }
 
-    internal class ChallengeSolver {}
+    internal class ChallengeSolver
+    {
+        private readonly string file = "";
+
+        public ChallengeSolver(string fileName)
+        {
+            file = fileName;
+        }
+
+        private string ReadFile
+        {
+            get { return File.ReadAllText(file); }
+        }
+    }
 
     internal class Program
     {
+        public string FileName = @"E:\Github\Coding Challenge 2\tls.txt";
         private static void Main(string[] args) {}
     }
 }
