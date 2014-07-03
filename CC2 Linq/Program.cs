9c7213c4cf2ba80b054be018e8899b4d187118ab
Dan-Andrei Gheorghe <Dan-Andrei.Gheorghe@softwire.com>
2014-07-03 14:45:00 +0100

    Linq version

---
+++ CC2 Linq/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CC2_Linq
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var readf = File.ReadAllText(@"E:\GitHub\Coding Challenge 2\CC2 Linq\tfl.txt").ToLower();
+            var range = Enumerable.Range(0, readf.Length - 2);
+            var subStrings = range.Select(element => readf.Substring(element, 3));
+            var filtered = subStrings.Where(item => (('a' <= item[0] && item[0] <= 'z') && ('a' <= item[1] && item[1] <= 'z') && ('a' <= item[2] && item[2] <= 'z')));
+            var distinct = filtered.Distinct();
+            foreach (var str in distinct)
+            {
+                Console.Write(str);
+                Console.Write(" ");
+                Console.WriteLine(filtered.Count(el => el == str));
+            }
+            Console.Read();
+        }
+    }
+}
